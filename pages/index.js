a363f7f22aa98d3c2d1a03f39f1ee7d880eeadf4
unknown <fahimismail75.scc@gmail.com>
2022-06-28 14:52:36 +0600

    prac

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Layout from '../components/Layout'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
 
-const Home = ({coffeeStores}) => {
+const Home = ({there}) => {
+    console.log(there)
     
     return(
         <>
@@ -14,12 +15,22 @@ const Home = ({coffeeStores}) => {
                 
                 <div className='h-[100vh] w-[100%] px-[8rem] bg-gradient-to-r from-purple-500 to-pink-500'>
                     <Testimonial />
+                    <h1>{there}</h1>
                 </div>
 
             </main>
         </Layout>
         </>
     )
+}
+
+export const getStaticProps = async () => {
+    
+    return {
+        props: {
+            there: 'sdad'
+        }
+    }
 }
 
 export default Home
